1b9615f10283f592fa5ca067b3c30da39be4aa27
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-12-05 00:59:07 +0300

    seminar_2

---
+++ seminar_2/Task_10/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число 
+ и на выходе показывает вторую цифру этого числа*/
+
+Console.WriteLine("Введите трёхзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if ((number >= 100) && (number <= 999))
+{
+   Console.WriteLine($"Вторая цифра числа = {(number / 10) % 10}"); 
+}
+else 
+   Console.Write("Допущена ошибка при вводе числа");
---
+++ seminar_2/Task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую 
+//  день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число, обозначающее день недели (1 - 7)");
+int day_week = Convert.ToInt32(Console.ReadLine());
+if ((day_week > 0) && (day_week < 6))
+{
+    Console.WriteLine("Будни...");
+}
+else if (day_week == 6 || day_week == 7) 
+{
+    Console.WriteLine("Выходной!");
+} 
+else Console.WriteLine("Ошибка при вводе числа!");

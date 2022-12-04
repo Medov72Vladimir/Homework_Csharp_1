c10c4508d8ce4fa226d68f1e671258ecc09e0bcf
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-12-04 22:34:43 +0300

    добавлено решение задачи№10

---
+++ Task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число 
+ и на выходе показывает вторую цифру этого числа*/
+
+Console.WriteLine("Введи трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+int res = (number / 10) %10;
+Console.WriteLine($" Вторая цифра числа = {res}");

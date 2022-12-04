a59c0cd3f05f0824d04e6dea9e082dc7a07ade60
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-12-04 23:50:30 +0300

    добавлено решение задачи№10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,8 +1,11 @@
 ﻿/* Напишите программу, которая принимает на вход трёхзначное число 
  и на выходе показывает вторую цифру этого числа*/
 
-Console.WriteLine("Введи трёхзначное число");
+Console.WriteLine("Введите трёхзначное число ");
 int number = Convert.ToInt32(Console.ReadLine());
 number = Math.Abs(number);
-int res = (number / 10) %10;
-Console.WriteLine($" Вторая цифра числа = {res}");
+if ((number >= 100) && (number <= 999))
+{
+   Console.WriteLine($"Вторая цифра числа = {(number / 10) % 10}"); 
+}
+else Console.Write("Допущена ошибка при вводе числа");

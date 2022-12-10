c45178d8afff14024d375d77e9d291809b3cedd2
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-12-11 01:19:58 +0300

    решение задачи №27

---
+++ seminar_4/Task_27/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Задача 27
+//  Напишите программу, которая принимает на вход число
+//  и выдаёт сумму цифр в числе.
+
+      Console.Write("Введите число -> ");
+      string? number = Convert.ToString(Console.ReadLine());
+      int[] array = new int[number!.Length];
+      int sum = 0;
+      int i;
+      for (i = 0; i < number.Length; i++)
+      {
+        array[i] = (int)char.GetNumericValue(number[i]);
+         sum += array[i]; 
+      }
+      Console.WriteLine(sum);
+   

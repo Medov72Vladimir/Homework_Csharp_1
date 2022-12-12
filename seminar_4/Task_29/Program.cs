9c21d052bcc3c39ab30a2dc4c2128bea62eb381c
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-12-12 21:03:15 +0300

    Решение задачи №29

---
+++ seminar_4/Task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите число, равное длине массива -> ");
+var longArray = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[longArray!];
+Random rnd = new Random();
+Console.Write("[");
+for (int i = 0; i < longArray; i++)
+{
+    array[i] = rnd.Next(1, 99);
+    Console.Write(array[i] + " " );
+}
+Console.Write("]");

e02811205fa8cf6ebf5f8e9b9bd9cf82c851d992
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-12-12 21:02:35 +0300

    Решение задачи №27

--- seminar_4/Task_27/Program.cs
+++ seminar_4/Task_27/Program.cs
@@ -2,15 +2,14 @@
 //  Напишите программу, которая принимает на вход число
 //  и выдаёт сумму цифр в числе.
 
-      Console.Write("Введите число -> ");
-      string? number = Convert.ToString(Console.ReadLine());
-      int[] array = new int[number!.Length];
-      int sum = 0;
-      int i;
-      for (i = 0; i < number.Length; i++)
-      {
-        array[i] = (int)char.GetNumericValue(number[i]);
-         sum += array[i]; 
-      }
-      Console.WriteLine($"Сумма цифр числа {number} = {sum}");
+Console.Write("Введите число -> ");
+string? number = Convert.ToString(Console.ReadLine());
+int[] array = new int[number!.Length];
+int sum = 0;
+for (int i = 0; i < number.Length; i++)
+{
+    array[i] = (int)char.GetNumericValue(number[i]);
+    sum += array[i]; 
+}
+Console.WriteLine($"Сумма цифр числа {number} = {sum}");
    

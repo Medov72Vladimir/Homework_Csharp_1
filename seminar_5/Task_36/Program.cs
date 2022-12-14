f1f2698ebc220d9096a5e6871367ad52f43d235f
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-12-14 22:44:33 +0300

    Решение задачи №36

---
+++ seminar_5/Task_36/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 36.
+// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Write("Введи число равное длине массива -> ");
+var longArray = Convert.ToUInt32(Console.ReadLine());
+int[] array = new int [longArray!];
+Random rnd = new Random();
+int sumNotEvenIndex = 0;
+Console.Write("Массив -> [ ");
+for (int i = 0; i < longArray; i++)
+{
+    array[i] = rnd.Next(-99, 99);
+    Console.Write(array[i] + " " );
+    if ( i % 2 > 0) sumNotEvenIndex += array[i];
+}
+Console.WriteLine("]"); 
+Console.WriteLine($"Сумма элементов с нечетным индексом -> {sumNotEvenIndex}");

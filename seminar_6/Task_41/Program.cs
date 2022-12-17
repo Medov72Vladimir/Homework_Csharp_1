8addc6f5651a6a85dda7de0c32087637a5e23d57
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-12-18 00:34:17 +0300

    Решение задачи №41

---
+++ seminar_6/Task_41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41.
+// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введи число M -> ");
+var numberM = Convert.ToUInt32(Console.ReadLine());
+int[] array = new int [numberM!];
+Random rnd = new Random();
+int positivNumber = 0;
+Console.Write("Сгенерированная последовательность чисел -> ( ");
+for (int i = 0; i < numberM; i++)
+{
+    array[i] = rnd.Next(-999, 999);
+    Console.Write(array[i] + " " );
+    if (array[i] > 0) positivNumber++;
+}
+Console.WriteLine(")"); 
+Console.WriteLine($"Количество положительных чисел -> {positivNumber}");

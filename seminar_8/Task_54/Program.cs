f6e74ae13e5316c25d5cefa6257e761256064733
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-12-25 03:30:53 +0300

    Решение задачи №54

---
+++ seminar_8/Task_54/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 54.
+Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int[,] array = new int[3, 5];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+     for (int j = 0; j < array.GetLength(1); j++)
+     {
+          for (int m = 0; m < array.GetLength(1) - 1; m++)
+            {
+              if(array[i, m] < array[i, m + 1])
+              {
+                int num = array[i, m + 1];
+                array[i, m + 1] = array[i, m]; 
+                array[i, m] = num; 
+              }
+            }
+    }
+}
+ for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }

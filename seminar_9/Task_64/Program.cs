c5a451f4fcd1ab632d1967c968c6d33da7925bb3
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-12-26 23:37:15 +0300

    Решение задачи №64

---
+++ seminar_9/Task_64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64.
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+
+Console.Write("Введите число N ->  ");
+int N = int.Parse(Console.ReadLine());
+string SequenceNumber(int n)
+{
+if (n > 1)
+{
+    Console.Write(n + ", ");
+     return SequenceNumber(n - 1);
+}
+else return n.ToString();
+}
+Console.WriteLine(SequenceNumber(N));

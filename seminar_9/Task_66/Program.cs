5b007e24b77d5377e7dff6ac95e7823e91cb04c2
Medov72Vladimir <119006643+Medov72Vladimir@users.noreply.github.com>
2022-12-26 23:37:39 +0300

    Решение задачи №66

---
+++ seminar_9/Task_66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66.
+// Задайте значения M и N. Напишите программу, которая найдёт
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+Console.Write("Введите число M ->  ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите число N ->  ");
+int N = int.Parse(Console.ReadLine());
+
+int SumsSequenceNumber(int m, int n)
+{
+    if (m == n) return n;
+    else return m + SumsSequenceNumber(m + 1, n);
+}
+Console.WriteLine($"Искомая сумма = {SumsSequenceNumber(M, N)}");
